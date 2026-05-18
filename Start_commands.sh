3b429b882aacf43ea96718d81eace2f68593a442
othmane j <othmane.jdidi.ensaf@gmail.com>
2026-05-18 05:40:55 +0200

    test cpp 07

--- Start_commands.sh
+++ Start_commands.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#cd /home/pi/RPICAM
+cd /home/pi/RPICAM
 
 sleep 30
-./last_capture_server --public-dir /var/lib/rpicam-http --port 8080 > /home/pi/log_cmd1.txt 2>&1 &
-./capture_on_demande   --mqtt-host 185.137.122.221   --mqtt-port 1883   --mqtt-topic rpicam/capture/request   --width 2304   --height 1296   --timeout-ms 700   --autofocus-mode auto   --autofocus-range normal --duration 1 --interval-ms 5 > /home/pi/log_cmd2.txt 2>&1 &
-commande3 > /home/pi/log_cmd3.txt 2>&1 &
+./last_capture_server --public-dir /var/lib/rpicam-http --port 8080  2>&1 &
+./capture_on_demande   --mqtt-host 185.137.122.221   --mqtt-port 1883   --mqtt-topic rpicam/capture/request   --width 2304   --height 1296   --timeout-ms 700   --autofocus-mode auto   --autofocus-range normal --duration 1 --interval-ms 5  2>&1 &
+./pending_sftp_sync_v2_start.sh  2>&1 &
+#> /home/pi/log_cmd3.txt
